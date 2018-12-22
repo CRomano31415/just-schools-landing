64195f8c987babe76778ed3acea5c58675702711
Claudia Romano <claudiaromano@Claudias-MacBook-Pro.local>
2018-12-22 13:50:44 -0600

    finished styling

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,12 +1,25 @@
 class EmailsController < ApplicationController
 
-	def new
-	end
-
-	def create
-	end 
 
 	def index
 	end
+
+	def new
+		@email = Email.new
+	end
+
+	def create
+		address = params[:email]["email"]
+
+		@email = Email.create(email: address)
+
+		if @email.save
+	  		flash[:notice] = "Email was saved."
+	  		redirect_to root_path 
+		else
+			flash.now[:alert] = "Oops, something happened on our end. Please try again."
+	  		redirect_to root_path
+		end
+	end 
 
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,20 @@
 class WelcomeController < ApplicationController
 
 	def index
+		@email = Email.new
 	end
+
+	def create
+		@email = Email.new
+		@email = Email.create(email: params[:email])
+
+		if @email.save
+	  		flash[:notice] = "Email was saved."
+	  		redirect_to root_path 
+		else
+			flash.now[:alert] = "Oops, something happened on our end. Please try again."
+	  		redirect_to root_path
+  		end
+	end 
 
 end
